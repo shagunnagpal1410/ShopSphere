878f917beb7be1cfbd3bb2a37e0389d7555d3060
shagunnagpal1410 <160056931+shagunnagpal1410@users.noreply.github.com>
2026-06-11 00:56:34 +0530

    Enhance fetch with logging for debugging
    Added console logs for API URL, response status, and received data.

--- Frontend/src/components/NewCollections/NewCollections.jsx
+++ Frontend/src/components/NewCollections/NewCollections.jsx
@@ -5,10 +5,21 @@ import { useState, useEffect } from 'react'
 const NewCollections = () => {
   const [new_collection, setnew_collection] = useState([])
   useEffect(() => {
+  console.log("API URL:", import.meta.env.VITE_API_URL);
+
   fetch(`${import.meta.env.VITE_API_URL}/newcollection`)
-    .then((res) => res.json())
-    .then((data) => setnew_collection(data));
+    .then((res) => {
+      console.log("Status:", res.status);
+      return res.json();
+    })
+    .then((data) => {
+      console.log("Received:", data);
+      setnew_collection(data);
+    })
+    .catch((err) => console.error("Fetch error:", err));
 }, []);
+
+console.log("State:", new_collection);
   return (
     <div className='bg-linear-to-b from-[#C4E2F5] from-0% to-[#e1ffea22] to-100% flex flex-col gap-2.5 pt-[5vh] pb-[10vh] items-center'>
       <h1 className='text-4xl  tracking-tight text-[#171717] font-semibold'>NEW COLLECTION</h1>

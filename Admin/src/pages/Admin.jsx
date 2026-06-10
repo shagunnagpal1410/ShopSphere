53fda89c3e4f56d222d129f0ba25a31f3d0a5f37
shagunnagpal1410 <160056931+shagunnagpal1410@users.noreply.github.com>
2026-06-10 23:48:35 +0530

    Refactor Admin routes to include default navigation

--- Admin/src/pages/Admin.jsx
+++ Admin/src/pages/Admin.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import './output.css'
 import Sidebar from '../components/Sidebar/Sidebar'
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route, Navigate } from "react-router-dom";
 import Addproduct from '../components/Addproduct/Addproduct'
 import Listproduct from '../components/Listproduct/Listproduct'
 const Admin = () => {
   return (
     <div className='flex flex-col sm:flex-row bg-[#4BB8FA] min-h-screen'>
       <Sidebar/>
-      <Routes>
-        <Route path='/addproduct' element={<Addproduct/>}></Route>
-        <Route path='/listproduct' element={<Listproduct/>}></Route>
-      </Routes>
+     
+
+<Routes>
+  <Route path="/" element={<Navigate to="/addproduct" />} />
+  <Route path="/addproduct" element={<Addproduct />} />
+  <Route path="/listproduct" element={<Listproduct />} />
+</Routes>
     </div>
   )
 }

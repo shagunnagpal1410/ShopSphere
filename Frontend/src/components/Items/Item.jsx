c7553a9e11977e2fc84961d8eac3f879e96dcc28
shagunnagpal1410 <shagunnagpalluvumaths@gmail.com>
2026-06-11 23:06:25 +0530

    changing frontend

--- Frontend/src/components/Items/Item.jsx
+++ Frontend/src/components/Items/Item.jsx
@@ -3,17 +3,18 @@ import './output.css'
 import {Link} from 'react-router-dom'
 const Item = (props) => {
   return (
-    <div className='h-[25vh] w-[40vw] sm:w-[20vw] sm:h-[45vh] hover:scale-105 transition-[0.6s] overflow-hidden flex flex-col border-b
-    border-b-[#4BB8FA] items-center justify-center bg-linear-to-b from-[#4BB8FA] to-white rounded-3xl'>
+    <div className='h-[28vh] w-[40vw] sm:w-[20vw] sm:h-[48vh] hover:scale-105 transition-[0.6s] overflow-hidden flex flex-col border-b border-b-[#4BB8FA] items-center justify-evenly bg-linear-to-b from-[#4BB8FA] to-white '>
+      <div>
       <Link to={`/product/${props.id}`}>
-        <img onClick={window.scrollTo(0,0)} src={props.image} className= 'rounded-2xl h-[15vh] w-[30vw] sm:w-[16vw] sm:h-[30vh] object-cover'></img>
+        <img onClick={window.scrollTo(0,0)} src={props.image} className= ' h-[15vh] w-[30vw] sm:w-[16vw] sm:h-[30vh] object-cover overflow-hidden'></img>
       </Link>
-      <p className='m-[6px 0px]'>{props.name}</p>
-      <div className="item-prices flex gap-5 ">
-        <div className="item-new-price text-[#374151] text-[18px] font-semibold">
+      </div>
+      <p className='h-[5vh] sm:h-[7.5vh] w-full m-[6px 0px] text-[10px] sm:text-[18px] text-center'>{props.name}</p>
+      <div className="item-prices flex gap-5 h-[5vh] sm:h-[7.5vh] w-full justify-center">
+        <div className="item-new-price text-[#374151] text-[10px] sm:text-[18px] font-semibold">
             Rs {props.new_price}
         </div>
-        <div className="item-old-price  text-[#8c8c8c] text-[18px] font-semibold line-through">
+        <div className="item-old-price  text-[#8c8c8c] text-[10px] sm:text-[18px] font-semibold line-through">
             Rs {props.old_price}
         </div>
       </div>
--- Frontend/src/components/Relatedproducts/Relatedproducts.jsx
+++ Frontend/src/components/Relatedproducts/Relatedproducts.jsx
@@ -14,10 +14,10 @@ const Relatedproducts = () => {
       .catch((err) => console.error(err));
   }, []);
   return (
-    <div className='flex flex-col items-center gap-2.5 pb-5'>
+    <div className='flex flex-col items-center gap-2.5 pb-10'>
       <h1 className='text-[#171717] text-[20px] sm:text-[50px] font-semibold '>Related Products</h1>
       <hr className='w-50 h-1.5 rounded-[10px] bg-[#252525]'></hr>
-      <div className="relatedproducts-item mt-12.5 flex sm:gap-6 flex-wrap justify-evenly">
+      <div className="relatedproducts-item mt-12.5 flex sm:gap-6 gap-y-6 flex-wrap justify-evenly">
         {relatedproducts.map((item, i) => {
           return <Item key={i} id={item.id} name={item.name} image={item.image} old_price={item.old_price} new_price={item.new_price} />
         })}

84fdf1fc604158396df07a189827b3da3a505cf6
shagunnagpal1410 <shagunnagpalluvumaths@gmail.com>
2026-06-18 15:42:07 +0530

    changes in items

--- Frontend/src/components/Items/Item.jsx
+++ Frontend/src/components/Items/Item.jsx
@@ -9,7 +9,7 @@ const Item = (props) => {
         <img onClick={window.scrollTo(0,0)} src={props.image} className= ' h-[15vh] w-[30vw] sm:w-[16vw] sm:h-[30vh] object-cover overflow-hidden'></img>
       </Link>
       </div>
-      <p className='h-[5vh] sm:h-[7.5vh] w-full m-[6px 0px] text-[10px] sm:text-[18px] leading-1 text-center'>{props.name}</p>
+      <p className='h-[5vh] sm:h-[7.5vh] w-full m-[6px 0px] text-[10px] sm:text-[18px] leading-1.5 text-center'>{props.name}</p>
       <div className="item-prices flex gap-5 h-[5vh] sm:h-[7.5vh] w-full justify-center">
         <div className="item-new-price text-[#374151] text-[10px] sm:text-[18px] font-semibold">
             Rs {props.new_price}
